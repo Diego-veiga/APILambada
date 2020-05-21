2ecf52a107464f6f8f92c4a13f9a7a0b58d06e1f
DiegoVeiga <diegoroberto27@hotmail.com>
2020-05-21 15:20:48 -0300

    Ajuste connectionstring

--- APILambada/Startup.cs
+++ APILambada/Startup.cs
@@ -66,10 +66,10 @@ namespace APILambada
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            //if (env.IsDevelopment())
+            
                 app.UseDeveloperExceptionPage();
-            }
+            
 
             app.UseHttpsRedirection();
 
