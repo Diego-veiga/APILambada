2de12fc4fa820880b6da64b801d8af1776f85bdf
DiegoVeiga <diegoroberto27@hotmail.com>
2020-05-21 15:04:25 -0300

    DOcumentação

--- APILambada/Startup.cs
+++ APILambada/Startup.cs
@@ -2,11 +2,14 @@ using APILambada.Data;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Linq;
 using System.Text;
 
 namespace APILambada
@@ -23,12 +26,19 @@ namespace APILambada
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
+            services.AddResponseCompression(op =>
+            {
+                op.Providers.Add<GzipCompressionProvider>();
+                op.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[] { "application/json" });
+            });
             services.AddControllersWithViews()
                     .AddNewtonsoftJson(options =>
             options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
 );
             //services.AddDbContext<LambadaContext>(op => op.UseSqlServer(Configuration.GetConnectionString("connectionStrngs")));
             services.AddDbContext<LambadaContext>(op => op.UseSqlServer(Configuration.GetConnectionString("connectionStrngs")));
+            
 
             services.AddControllers();
             var key = Encoding.ASCII.GetBytes(Settings.Secret);
@@ -49,6 +59,8 @@ namespace APILambada
                 };
 
             });
+            services.AddSwaggerGen(c =>
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Api da Lambada", Version = "1.0" }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -62,6 +74,15 @@ namespace APILambada
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Lambada Api V1");
+            });
+            app.UseCors(X => X.
+                              AllowAnyOrigin()
+                             .AllowAnyMethod()
+                             .AllowAnyHeader());
             app.UseAuthentication();
             app.UseAuthorization();
 
